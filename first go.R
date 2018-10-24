b47ae188e6996fa3440f439e44bede7c610b909b
Artemis <artie.link@gmail.com>
2018-10-24 08:01:25 -0600

    Delete first go.R

--- first go.R
+++
@@ -1,3 +0,0 @@
-y=read.csv("file:///C:/Users/Lucifer/Google Drive (artielink@gmail.com)/gitty-huby/It-s-like---it-s-like-a-yeti/Berberis Stems_Pitfall traps.csv")
-g=write.csv(y,"file:///C:/Users/Lucifer/Google Drive (artielink@gmail.com)/gitty-huby/It-s-like---it-s-like-a-yeti/Berberis Stems_Pitfall traps.csv")
-
